c5f3f469bdc5d1da4b8f9f068c906d173fd7745e
Jordan Leigh <AlwaysBCoding@gmail.com>
2012-12-05 14:10:35 -0600

    The game now prevents you from taking an already taken square

--- tictactoe.rb
+++ tictactoe.rb
@@ -8,11 +8,18 @@ while !board.game_over?
 
   print "\nChoose a number to place an 'X' there >> "
   user_move = gets.chomp
-  chosen_square = board.detect_square(user_move)
+  
+#  p board.empty_squares.find { |sq| sq.text_value == user_move.to_i }
+  if board.empty_squares.find { |sq| sq.text_value == user_move.to_i }
+    chosen_square = board.detect_square(user_move)
 
-  board.make_user_move(chosen_square)
-  board.make_computer_move(board.calculate_computer_response(chosen_square)) unless board.empty_squares.count == 0
-  board.draw()
+    board.make_user_move(chosen_square)
+    board.make_computer_move(board.calculate_computer_response(chosen_square)) unless board.empty_squares.count == 0
+    board.draw()
+  else 
+    puts "\nThat square has already been taken, what are you trying to pull?\n"
+    board.draw()
+  end
 
 end
 
