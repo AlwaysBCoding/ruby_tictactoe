3d9d5f5217b32c23477cdf8a0429908b508fb964
Jordan Leigh <AlwaysBCoding@gmail.com>
2012-12-10 10:04:01 -0600

    Ruby game now works with the game class abstraction

--- tictactoe.rb
+++ tictactoe.rb
@@ -12,7 +12,7 @@ while !game.over?
   if game.board.empty_squares.find { |sq| sq.text_value == user_move.to_i }
     chosen_square = game.board.detect_square(user_move)
     game.human.make_move(chosen_square)
-    game.computer.make_move(game.board.calculate_computer_response(game.board, chosen_square)) unless game.board.empty_squares.count == 0
+    game.computer.make_move(game.computer.calculate_response(game, chosen_square)) unless game.board.empty_squares.count == 0
   else 
     puts "\nThat square has already been taken, what are you trying to pull?\n"
   end
